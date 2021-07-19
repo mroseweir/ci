de14043c5c7ab8f3a533e57007db6784f5b6169b
mroseweir <roseweirmichael@gmail.com>
2021-07-19 15:00:23 -0500

    adds tests

--- project.js
+++ project.js
@@ -1,5 +1,13 @@
-function getGreeting(name){
+getGreeting = (name) => {
     return "Hi " + name;
-}
+};
 
-module.exports = getGreeting;
+findSum = (num1, num2) => {
+    return num1 + num2;
+};
+
+quickMafs = (num1, num2) => {
+    return num1 + num2;
+};
+  
+  module.exports = { getGreeting, findSum, quickMafs };
--- test.js
+++ test.js
@@ -1,5 +1,16 @@
-const getGreeting = require('./project');
+ 
+  const { getGreeting, findSum, quickMafs } = require("./project");
+  
+  test("return the sum of two numbers", () => {
+    console.log(findSum(3, 4));
+    expect(findSum(3, 4)).toBe(7);
+  });
+  
+  test("can create a basic greeting", () => {
+    expect(getGreeting("Sam")).toBe("Hi Sam");
+  });
 
-test('can create a basic greeting', () => {
-  expect(getGreeting('Sam')).toBe("Hi Sam");
-});
+  test('Return the sum of num1 and num2', () => {
+    console.log(quickMafs(1,2))
+    expect(quickMafs(1, 2)).toBe(3)
+  });
